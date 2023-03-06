a2683ee122bc7f863697e598d3203e88253f1793
All1in <bm3023068@gmail.com>
2023-03-06 16:41:07 +0200

    first one

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import './styles/App.css';
+
+const App: FC = () => {
+  return (
+    <div className="App">
+        <div className="header"></div>
+        <div className="todoList"></div>
+    </div>
+  );
+}
+
+export default App;
